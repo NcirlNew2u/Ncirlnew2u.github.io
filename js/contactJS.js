bb4a4e442b99500327b71220fef96783aac8d618
New2U <76208026+NcirlNew2u@users.noreply.github.com>
2020-12-19 14:53:06 +0000

    Update contactJS.js

--- js/contactJS.js
+++ js/contactJS.js
@@ -1,22 +1,29 @@
-//Creating the validation function which checks email and username. Sends confirmation of message being sent
+//Creating the validation function which checks email.
 function validate() {
-	var email = document.getElementById("email");
-	var name = document.getElementById("name");
-
+	var form = document.getElementById("contact-form");
+	var email = document.getElementById("email").value;
+	var text = document.getElementById("text");
+	var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 	
 	if(email.value == "") {
-		alert("Email cannot be blank");
-		//Return false stops the form submitting and bypassing the alert. Alerts the user then resets the form
-		return false;
-	}
-	if(name.value == "") {
-		alert("Name cannot be blank");
-		//Return false stops the form submitting and bypassing the alert. Alerts the user then resets the form
-		return false;
+		form.classList.remove ("valid");
+		form.classList.remove ("invalid");
+		text.innerHTML = "";
+		text.style.color = "black";
 	}
 	
-	else {
-		alert("Message has been sent");
-		return false;
+	if(email.match(pattern)) {
+		form.classList.add ("valid");
+		form.classList.remove ("invalid");
+		text.innerHTML = "Your email address is valid";
+		text.style.color = "#00ff00";
 	}
+	else {
+		form.classList.remove ("valid");
+		form.classList.add ("valid");
+		text.innerHTML = "Please enter valid email address";
+		text.style.color = "#ffffff";
+		
+	}	
+
 }
